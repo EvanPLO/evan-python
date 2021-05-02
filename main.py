ed06be8deecc99f7acfd9397a18f3ba8ccbafa4a
Evan Owens <dowens@DESKTOP-KEAN7A7.localdomain>
2021-05-02 12:07:38 +0100

    Evan's first code push

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello World")
+
+# this is a variable
+name = "Evan"
+print(f"Hello my name is {name}")
+
+a = 1
+b = 2
+sum = a + b 
+print(sum)
+
+items = ["rice", "ice cream", "spinich", "chicken nuggets"]
+print("my shopping list:")
+for item in items:
+    print(item)
